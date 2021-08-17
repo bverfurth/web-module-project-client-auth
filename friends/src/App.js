4dcbd0b2ac2c1da75065589564a01c264fb54992
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-08-17 18:30:14 -0400

    Changed app.js around. added axios

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,54 +1,43 @@
+import "./App.css";
 import React from "react";
 import Login from "./components/Login";
+import Logout from "./components/Logout";
 import FriendForm from "./components/FriendForm";
-import FriendList from "./components/FriendList";
+import FriendsList from "./components/FriendsList";
 import PrivateRoute from "./components/PrivateRoute";
-import axiosWithAuth from "./authorization/axiosWithAuth";
 import FriendsLogo from "../src/assets/FriendsLogo.png";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-import "./App.css";
-
-function App(props) {
-  const logout = () => {
-    localStorage.removeItem("token");
-  };
-
-  const isAuth = localStorage.getItem("token");
-
+function App() {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <div class="headerImage">
-            <img src={FriendsLogo} alt="Friends Logo" />
-            <h1></h1>
-          </div>
-          <ul>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              {!isAuth ? <Link to="/friends">Friends</Link> : <div></div>}
-            </li>
-            <li>
-              {!isAuth ? (
-                <Link to="/friends/form">Friends Form</Link>
-              ) : (
-                <div></div>
-              )}
-            </li>
-            <li>
-              <Link onClick={logout}>Logout</Link>
-            </li>
-          </ul>
-        </header>
+        <h1>
+          {" "}
+          <img src={FriendsLogo} alt="Friends Logo" />{" "}
+        </h1>
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/logout">Logout</Link>
+          </li>
+          <li>
+            {localStorage.getItem("token") && (
+              <div>
+                <Link to="/protected">Protected Page</Link>
+              </div>
+            )}
+          </li>
+        </ul>
 
         <Switch>
           <PrivateRoute exact path="/friends/form" component={FriendForm} />
-          <PrivateRoute exact path="/friends" component={FriendList} />
+          <PrivateRoute exact path="/protected" component={FriendsList} />
+          <PrivateRoute path="/logout" component={Logout} />
           <Route path="/login" component={Login} />
-          <Route component={Login} />
+          <Route path="/" component={Login} />
         </Switch>
       </div>
     </Router>
